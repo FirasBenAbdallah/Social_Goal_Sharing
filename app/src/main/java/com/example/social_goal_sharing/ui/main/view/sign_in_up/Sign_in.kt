76411223f7e675272cb7e3d3615b05618a16b8e2
Firasbenabdallah <firas.benabdallah@esprit.tn>
2022-11-10 19:54:01 +0100

    btnlogin

--- app/src/main/java/com/example/social_goal_sharing/ui/main/view/sign_in_up/Sign_in.kt
+++ app/src/main/java/com/example/social_goal_sharing/ui/main/view/sign_in_up/Sign_in.kt
@@ -2,11 +2,17 @@ package com.example.social_goal_sharing.ui.main.view.sign_in_up
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.social_goal_sharing.R
 
 class Sign_in : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+
+        val btnlogin = findViewById<Button>(R.id.btnlogin)
+        btnlogin.setOnClickListener{
+
+        }
     }
 }

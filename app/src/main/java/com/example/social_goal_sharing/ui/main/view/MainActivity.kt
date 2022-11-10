406536bccdaecd4ff7f2b60771b145ce086109e7
Firasbenabdallah <firas.benabdallah@esprit.tn>
2022-11-10 16:01:43 +0100

    Initial commit

---
+++ app/src/main/java/com/example/social_goal_sharing/ui/main/view/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.social_goal_sharing.ui.main.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.social_goal_sharing.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/social_goal_sharing/ui/main/view/sign_in_up/Sign_in.kt
@@ -0,0 +1,12 @@
+package com.example.social_goal_sharing.ui.main.view.sign_in_up
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.social_goal_sharing.R
+
+class Sign_in : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}

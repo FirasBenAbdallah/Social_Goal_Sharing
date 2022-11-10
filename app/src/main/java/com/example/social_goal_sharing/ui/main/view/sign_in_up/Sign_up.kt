f0bd67f1309708aba3c58a53780decd699726640
abdesslemchebili <abdesslem.chebili@esprit.tn>
2022-11-10 20:07:17 +0100

    signup

---
+++ app/src/main/java/com/example/social_goal_sharing/ui/main/view/sign_in_up/Sign_up.kt
@@ -0,0 +1,11 @@
+package com.example.social_goal_sharing.ui.main.view.sign_in_up
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Sign_up : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}

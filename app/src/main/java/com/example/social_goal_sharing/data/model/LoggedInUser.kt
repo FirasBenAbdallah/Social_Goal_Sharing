2266205455b9e334e2528a6a83fbf21130d51d90
Firasbenabdallah <firas.benabdallah@esprit.tn>
2022-11-10 23:08:05 +0100

    Button_Login & Text_sign_up startActivity

--- app/src/main/java/com/example/social_goal_sharing/data/model/LoggedInUser.kt
+++
@@ -1,9 +0,0 @@
-package com.example.social_goal_sharing.data.model
-
-/**
- * Data class that captures user information for logged in users retrieved from LoginRepository
- */
-data class LoggedInUser(
-    val userId: String,
-    val displayName: String
-)
--- app/src/main/java/com/example/social_goal_sharing/ui/main/view/sign_in_up/Sign_in.kt
+++ app/src/main/java/com/example/social_goal_sharing/ui/main/view/sign_in_up/Sign_in.kt
@@ -1,18 +1,27 @@
 package com.example.social_goal_sharing.ui.main.view.sign_in_up
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import com.example.social_goal_sharing.R
+import com.example.social_goal_sharing.ui.main.view.MainActivity
 
 class Sign_in : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
 
+        val textviewsignup = findViewById<TextView>(R.id.textViewSignUp)
         val btnlogin = findViewById<Button>(R.id.btnlogin)
-        btnlogin.setOnClickListener{
 
+        btnlogin.setOnClickListener{
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+        textviewsignup.setOnClickListener(){
+            startActivity(Intent(this, Sign_up::class.java))
         }
     }
 }

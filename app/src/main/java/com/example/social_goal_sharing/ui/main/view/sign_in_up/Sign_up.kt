f4039ed2c0e0cfc203bed1b6d3230542161d6fac
abdesslemchebili <abdesslem.chebili@esprit.tn>
2022-11-11 14:17:29 +0100

    logo

--- app/src/main/java/com/example/social_goal_sharing/ui/main/view/sign_in_up/Sign_up.kt
+++ app/src/main/java/com/example/social_goal_sharing/ui/main/view/sign_in_up/Sign_up.kt
@@ -1,12 +1,21 @@
 package com.example.social_goal_sharing.ui.main.view.sign_in_up
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.social_goal_sharing.R
+import com.example.social_goal_sharing.ui.main.view.MainActivity
 
 class Sign_up : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        val btnsignup = findViewById<Button>(R.id.btnsignup)
+
+        btnsignup.setOnClickListener{
+            startActivity(Intent(this, Sign_in::class.java))
+        }
     }
 }

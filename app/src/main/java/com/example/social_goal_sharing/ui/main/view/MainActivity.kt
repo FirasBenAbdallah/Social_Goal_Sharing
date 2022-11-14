debefeda6b60963ed83b67164eaef3b8fad843ec
abdesslemchebili <abdesslem.chebili@esprit.tn>
2022-11-14 02:23:15 +0100

    bottom navigation bar

--- app/src/main/java/com/example/social_goal_sharing/ui/main/view/MainActivity.kt
+++ app/src/main/java/com/example/social_goal_sharing/ui/main/view/MainActivity.kt
@@ -8,22 +8,53 @@ import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
 import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
 import com.example.social_goal_sharing.R
+import com.example.social_goal_sharing.databinding.ActivityMainBinding
 import com.example.social_goal_sharing.ui.main.view.sign_in_up.Sign_up
 import com.example.social_goal_sharing.ui.main.view.sign_in_up.Sign_in
+import com.example.social_goal_sharing.ui.main.view.toolbar_fragments.*
 
 class MainActivity : AppCompatActivity() {
-
     @SuppressLint("MissingInflatedId")
+    private lateinit var binding: ActivityMainBinding
+    private fun replaceFragment(fragment: Fragment){
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame,fragment)
+        fragmentTransaction.commitNow()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(Home())
+
+//        setContentView(R.layout.activity_main)
+        binding.bottomnavigationtoolbar.setOnItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.homeIcon -> replaceFragment(Home())
+                R.id.profilIcon -> replaceFragment(profile())
+                R.id.plusIcon -> replaceFragment(Add())
+                R.id.searchIcon -> replaceFragment(LookFor())
+                R.id.messageIcon -> replaceFragment(Discussion())
+                else -> {
+
+                }
+
+            }
+            true
+        }
 
         val btnsignup = findViewById<Button>(R.id.btnsignup1)
         val btnsignin = findViewById<Button>(R.id.btnsignin)
-        val toolBar = findViewById<Toolbar>(R.id.bottomnavigationtoolbar)
+//        val toolBar = findViewById<Toolbar>(R.id.bottomnavigationtoolbar)
 
-        setSupportActionBar(toolBar)
+//        setSupportActionBar(toolBar)
+
 
         btnsignup.setOnClickListener {
             startActivity(Intent(this, Sign_up::class.java))
@@ -31,18 +62,21 @@ class MainActivity : AppCompatActivity() {
         btnsignin.setOnClickListener{
             startActivity(Intent(this, Sign_in::class.java))
         }
+
     }
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
+
+   /* override fun onCreateOptionsMenu(menu: Menu?): Boolean {
         menuInflater.inflate(R.menu.profile, menu)
         return super.onCreateOptionsMenu(menu)
     }
-
-    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+*/
+    /*override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         R.id.disconnectIcon -> {
             startActivity(Intent(this, Sign_in::class.java))
             finishAffinity()
             true
         }
         else -> super.onOptionsItemSelected(item)
-    }
+    }*/
 }

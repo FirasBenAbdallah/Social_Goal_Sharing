ad8acdf4a3e0b673127fcff4ba5580b8265a6e08
abdesslemchebili <abdesslem.chebili@esprit.tn>
2022-11-15 09:55:17 +0100

    bottom navigation bar

---
+++ app/src/main/java/recycler/Acc.kt
@@ -0,0 +1,3 @@
+package recycler
+
+data class Acc(var profileIcon: Int , var imgPub : Int , var linear : String , var linearPub : Int, var liked : String)
---
+++ app/src/main/java/recycler/HomeAdapter.kt
@@ -0,0 +1,45 @@
+package recycler
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.social_goal_sharing.R
+import com.google.android.material.imageview.ShapeableImageView
+import org.w3c.dom.Text
+
+class HomeAdapter(private val accList : ArrayList<Acc>): RecyclerView.Adapter<recycler.HomeAdapter.MyViewHolder>() {
+
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentItem =accList[position]
+        holder.profileIcon.setImageResource(currentItem.profileIcon)
+        holder.imgpub.setImageResource(currentItem.imgPub)
+        holder.linear.layout(currentItem.linear)
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    class MyViewHolder (itemView : View):RecyclerView.ViewHolder(itemView){
+
+val profileIcon :ShapeableImageView = itemView.findViewById(R.id.profilIcon)
+val imgpub :ShapeableImageView = itemView.findViewById(R.id.imgpub)
+
+        val linear : View = itemView.findViewById(R.id.linear1)
+        val linearPub : View = itemView.findViewById(R.id.linearPub)
+        val liked : TextView = itemView.findViewById(R.id.liked)
+
+
+
+
+    }
+
+
+}

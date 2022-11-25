56887a453cbb84a94f9ac3c14f25863b65567575
abdesslemchebili <abdesslem.chebili@esprit.tn>
2022-11-25 17:56:43 +0100

    calendrier

--- app/src/main/java/com/example/social_goal_sharing/ui/main/view/toolbar_fragments/Add.kt
+++ app/src/main/java/com/example/social_goal_sharing/ui/main/view/toolbar_fragments/Add.kt
@@ -5,28 +5,41 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.DatePicker
+import android.widget.ImageButton
+import android.widget.TextView
 import com.example.social_goal_sharing.R
+import com.example.social_goal_sharing.databinding.ActivityMainBinding
+import com.example.social_goal_sharing.databinding.FragmentAddBinding
+import com.google.android.material.datepicker.MaterialDatePicker
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [Add.newInstance] factory method to
  * create an instance of this fragment.
  */
-class Add : Fragment() {
+class Add : Fragment(R.layout.fragment_add) {
+     private  var _binding: FragmentAddBinding? = null
+     private val binding get() = _binding !!
+    val dateText = view?.findViewById<TextView>(R.id.dateEv)
+
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -35,8 +48,11 @@ class Add : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add, container, false)
+        _binding = FragmentAddBinding.inflate(inflater,container,false)
+        val view = binding.root
+        return view
     }
+
 
     companion object {
         /**
@@ -56,5 +72,23 @@ class Add : Fragment() {
                     putString(ARG_PARAM2, param2)
                 }
             }
+    }
+    private fun showDateRange() {
+        val dateRangePicker = MaterialDatePicker.Builder.dateRangePicker()
+            .setTitleText("Select Date")
+            .build()
+
+        dateRangePicker.show(
+            parentFragmentManager,
+            "date_range_picker"
+        )
+        dateRangePicker.addOnPositiveButtonClickListener { datePicked->
+            val startDate = datePicked.first
+            val endDate = datePicked.second
+            dateText?.text ?:  "StartDate: $startDate" +"\nEndDate : $endDate"
+
+
+
+        }
     }
 }

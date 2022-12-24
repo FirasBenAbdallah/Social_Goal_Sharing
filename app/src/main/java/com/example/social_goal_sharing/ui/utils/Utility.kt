45850211fd6bec7c97981163c35b3c74f1fb9b86
Firasbenabdallah <firas.benabdallah@esprit.tn>
2022-12-24 01:38:33 +0100

    Utility

--- app/src/main/java/com/example/social_goal_sharing/ui/utils/Utility.kt
+++ app/src/main/java/com/example/social_goal_sharing/ui/utils/Utility.kt
@@ -4,7 +4,7 @@ import android.app.AlertDialog
 import android.content.Context
 
 object Utility {
-    val apiUrl: String = "http://192.168.1.9:3000"
+    val apiUrl: String = "http://192.168.1.15:3000"
     fun showAlert(
         context: Context, title: String = "",
         message: String = "", onYes: Runnable? = null, onNo: Runnable? = null
@@ -12,10 +12,10 @@ object Utility {
         val alertDialogBuilder = AlertDialog.Builder(context)
         alertDialogBuilder.setTitle(title)
         alertDialogBuilder.setMessage(message)
-        alertDialogBuilder.setPositiveButton(android.R.string.yes) { dialog, which ->
+        alertDialogBuilder.setPositiveButton("yes") { dialog, which ->
             onYes?.run()
         }
-        alertDialogBuilder.setNegativeButton(android.R.string.no) { dialog, which ->
+        alertDialogBuilder.setNegativeButton("no") { dialog, which ->
             onNo?.run()
         }
         alertDialogBuilder.show()
